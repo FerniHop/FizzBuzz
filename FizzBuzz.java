87297d541fe89afa758d1f68ca3a2cc9aec6037f
Alexandre Gossard <119630621+FerniHop@users.noreply.github.com>
2023-05-14 23:13:28 +0200

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -4,10 +4,10 @@ public class FizzBuzz{
     public static void main(String[] args) {
         for(int i = 1; i <= 30; i++){
             if((i%3 == 0) & (i%5 == 0)){
-            System.out.println("FizzBuzz.");continue;}
+            System.out.println("FizzBuzz");continue;}
 
-            if(i%3 == 0){System.out.println("Fizz.");continue;}
-            if(i%5 == 0){System.out.println("Buzz.");continue;}
+            if(i%3 == 0){System.out.println("Fizz");continue;}
+            if(i%5 == 0){System.out.println("Buzz");continue;}
             System.out.println(i);
         }
     }
